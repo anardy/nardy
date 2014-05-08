d1b7babaa9e26db85ecca8f6a17b5083f47e0ebc
anardy <macknardy@gmail.com>
2014-05-07 21:51:09 -0300

    alteracao dos arquivos Gruntfile e package

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,12 @@
 module.exports = function(grunt){
 	
-	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
-    
-	grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-		htmlhint: {
-			build: {
-				options: {
-					'tag-pair': true,
-					'tagname-lowercase': true,
-					'attr-lowercase': true,
-					'attr-value-double-quotes': true,
-					'doctype-first': true,
-					'spec-char-escape': true,
-					'id-unique': true,
-					'head-script-disabled': true,
-					'style-disabled': true
-				},
-			src: ['index.html']
-			}
-		},
-		watch: {
-			html: {
-				files: ['index.html'],
-				tasks: ['htmlhint']
-			}
-		},
-		uglify: {
-			build: {
-				files: {
-					'build/js/base.min.js': ['assets/js/base.js']
-				}
-			}
-		},
+  	grunt.initConfig({
 		jshint: {
 			all: ['assets/js/*.js']
 		}
     });
-
-    grunt.registerTask('default', []);
+	
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', 'jshint');
 
 };
